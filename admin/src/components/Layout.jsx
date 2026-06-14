9d41f8bfe66959792559303286e9bac0ee10ab74
anneline-codes <lineanne65@gmail.com>
2026-06-14 07:39:10 +0200

    Made some changes

--- admin/src/components/Layout.jsx
+++ admin/src/components/Layout.jsx
@@ -1,5 +1,5 @@
 import { Outlet } from 'react-router-dom';
-import Sidebar from './Sidebar' ;
+import Sidebar from './Sidebar';
 import { User } from 'lucide-react';
 import useAuthStore from '../store/authStore';
 
